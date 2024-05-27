474c09d745a6a19099a4e563d59a759475dfd83f
wms <egerromaan@gmail.com>
2024-05-27 12:55:07 +0600

    init tests

--- BullsAndCows/src/main/java/tests/GameTestCases.java
+++ BullsAndCows/src/main/java/tests/GameTestCases.java
@@ -1,4 +1,18 @@
 package tests;
 
+import org.example.Bulls;
+import org.example.Game;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameTestCases {
+
+    @Test
+    public void Test1(){
+        int[] arrBulls = new int[]{4, 3, 1, 2};
+        Bulls bulls = new Bulls(arrBulls, true);
+        Game game = new Game();
+        int result = game.gameVsBot(bulls);
+        assertEquals(0, result);
+    }
 }
