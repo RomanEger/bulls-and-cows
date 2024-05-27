8d1706cfa965a4cfca194f7498911950dd185007
wms <egerromaan@gmail.com>
2024-05-27 12:53:23 +0600

    add game

--- BullsAndCows/src/main/java/org/example/Game.java
+++ BullsAndCows/src/main/java/org/example/Game.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Game {
+    public int gameVsBot(Bulls bulls){
+        int steps = 0;
+
+        int[] arrBulls = bulls.getBulls();
+
+        return steps;
+    }
+
+    public int gameVsPlayer(Bulls firstBulls, Bulls secondBulls){
+        int winner = 0;
+
+        return winner;
+    }
 }
